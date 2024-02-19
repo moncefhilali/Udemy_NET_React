73a4dbdafb0c825d02c1d0039d382edf6b79be51
moncefhilali <moncefelhilali@gmail.com>
2024-02-19 13:58:07 +0100

    End of Section 3

--- Udemy.API/Program.cs
+++ Udemy.API/Program.cs
@@ -9,9 +9,20 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// SQLite
 builder.Services.AddDbContext<DataContext>(opt=>
 {
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+// Cors
+builder.Services.AddCors(opt =>
+{
+    opt.AddPolicy("CorsPolicy", policy =>
+    {
+        policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");
+    });
 });
 
 var app = builder.Build();
@@ -22,6 +33,9 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+// Cors
+app.UseCors("CorsPolicy");
 
 app.UseAuthorization();
 
