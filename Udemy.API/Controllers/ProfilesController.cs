14a7e9ab91c4514507fdcdc117e2dd8df4fbe35f
Moncef ElHilali <moncef.elhilali@americaneagle.com>
2024-02-26 04:45:15 -0600

    End Section 16

---
+++ Udemy.API/Controllers/ProfilesController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Udemy.Application.Profiles;
+
+namespace Udemy.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProfilesController : BaseApiController
+    {
+        [HttpGet("{username}")]
+        public async Task<IActionResult> GetProfile(string username)
+        {
+            return HandleResult(await Mediator.Send(new Details.Query { Username = username}));
+
+        }
+    }
+}
--- Udemy.Application/Activities/AttendeeDto.cs
+++ Udemy.Application/Activities/AttendeeDto.cs
@@ -1,14 +1,16 @@
-﻿using Udemy.Domain;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-namespace Udemy.Application.Profiles
+namespace Udemy.Application.Activities
 {
-    public class Profile
+    public class AttendeeDto
     {
         public string Username { get; set; }
         public string DisplayName { get; set; }
         public string Bio { get; set; }
         public string Image { get; set; }
-
-        public ICollection<Photo> Photos { get; set; }
     }
 }
